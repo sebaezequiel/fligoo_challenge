87eab120bd8244f6b552c79cc33be219e538dcf2
Sebastian_Bejarano_96 <34950845+sebaezequiel@users.noreply.github.com>
2025-09-07 00:41:46 -0300

    Update init.sql

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,24 +1,15 @@
---CREATING DATABASE
-CREATE DATABASE testfligoo;
-
---CONECTING TO TESTFLIGOO AND CREATING TABLE
-\connect testfligoo;
-
---CREATING TABLE
 CREATE TABLE IF NOT EXISTS testdata (
-  id SERIAL PRIMARY KEY,
-  flight_date DATE,
-  flight_status TEXT,
-  departure_airport TEXT,
-  departure_timezone TEXT,
-  arrival_airport TEXT,
-  arrival_timezone TEXT,
-  arrival_terminal TEXT,
-  airline_name TEXT,
-  flight_number TEXT,
-  ingested_at TIMESTAMP DEFAULT NOW()
+  flight_date        date        NOT NULL,
+  flight_status      text,
+  departure_airport  text        NOT NULL,
+  departure_timezone text,
+  arrival_airport    text        NOT NULL,
+  arrival_timezone   text,
+  arrival_terminal   text,
+  airline_name       text        NOT NULL,
+  flight_number      text        NOT NULL,
+  ingested_at        timestamptz NOT NULL DEFAULT now()
 );
 
---CREATING INDEX TO AVOID DUPLICATES
 CREATE UNIQUE INDEX IF NOT EXISTS ux_testdata_natural
-ON testdata (flight_date, flight_number, airline_name, departure_airport, arrival_airport);
+  ON testdata (flight_date, flight_number, airline_name, departure_airport, arrival_airport);
